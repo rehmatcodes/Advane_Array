4a8a40522f486743ad55f28a3af305545b60c193
rehmat ullah <rehmat.codes@gmail.com>
2024-09-06 10:42:22 -0700

    array advance

---
+++ app.js
@@ -0,0 +1,38 @@
+const myArr = []
+// %DebugPrint(myArr)
+
+const ArrTwo = [1,2,3,4,5]
+
+ArrTwo.push(6.0)
+
+ArrTwo.push("7")
+
+ArrTwo[10] = 11
+
+console.log(ArrTwo);
+console.log(ArrTwo.length);
+console.log(ArrTwo[9]);
+
+ const  ArrThree = [1,2,3,4,5]
+ console.log(ArrThree[8]);
+
+
+ const ArrFour = new Array(3)
+
+ ArrFour[0] = '1'
+  ArrFour[1] = '2'
+   ArrFour[2] = '3'
+
+   const ArrFive = []
+
+   ArrFive.push['1']
+   ArrFive.push['2']
+   ArrFive.push['3']
+
+   const Arrsix = [1,2,3,4,5]
+
+
+   Arrsix.push[isFinite]
+ 
+
+
